e9ffbd7ded18a24596ce621d67dfb6706871d13d
Faishalbadri <faishalalbadri1@gmail.com>
2022-04-16 12:44:08 +0700

    delete dummy data

--- app/src/main/java/com/faishalbadri/notepad/repository/splashscreen/SplashScreenDataLocal.java
+++ app/src/main/java/com/faishalbadri/notepad/repository/splashscreen/SplashScreenDataLocal.java
@@ -1,5 +1,7 @@
 package com.faishalbadri.notepad.repository.splashscreen;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.faishalbadri.notepad.api.local.RoomClient;
@@ -22,27 +24,7 @@ public class SplashScreenDataLocal implements SplashScreenDataResource {
         int size = database.roomInterface().getNotesAll().size();
 
         if (size == 0) {
-            DataNotes dataNotes = new DataNotes();
-            dataNotes.setNotes_title("Catatan faishal 1");
-            dataNotes.setNotes_desc("Catatan Hadist Kiamat Besar asdasd asdas asdas asdas asdasd as ds ad sa asdasdasdas asdassadas as dasdasdsad asdasdasdas asdasdasdasd asdasdasdas aasdasd asdasdasdsa asdsa asdasdasd asdsadsa asdasdas sadasdasd asdasdas asdasda asdasdsa asdsaa");
-            dataNotes.setDates(Calendar.getInstance().getTime());
-            dataNotes.setPinned(0);
-            database.roomInterface().addNotes(dataNotes);
-            dataNotes.setNotes_title("Catatan udin 1");
-            dataNotes.setNotes_desc("Catatan Hadist Kiamat Besar asdasd asdas asdas asdas asdasd as ds ad sa asdasdasdas asdassadas as dasdasdsad asdasdasdas asdasdasdasd asdasdasdas aasdasd asdasdasdsa asdsa asdasdasd asdsadsa asdasdas sadasdasd asdasdas asdasda asdasdsa asdsaa");
-            dataNotes.setDates(Calendar.getInstance().getTime());
-            dataNotes.setPinned(1);
-            database.roomInterface().addNotes(dataNotes);
-            dataNotes.setNotes_title(" Catatan mamat 2");
-            dataNotes.setNotes_desc("Catatan Hadist Kiamat Besar asdasd asdas asdas dasdasd asdasdasdas aasdasd asdasdasdsa asdsa asdasdasd asdsadsa asdasdas sadasdasd asdasdas asdasda asdasdsa asdsaa");
-            dataNotes.setDates(Calendar.getInstance().getTime());
-            dataNotes.setPinned(0);
-            database.roomInterface().addNotes(dataNotes);
-            dataNotes.setNotes_title("Catatan Hadist Kiamat Besar dan Kiamat kecil serta contoh contohnya 2");
-            dataNotes.setNotes_desc("Catatan Hadist Kiamat Besar asdasd asdas asdas asdas asdasd as ds ad sa");
-            dataNotes.setDates(Calendar.getInstance().getTime());
-            dataNotes.setPinned(1);
-            database.roomInterface().addNotes(dataNotes);
+            Log.i("dummy", "delete dummy data");
         }
         notesGetCallback.onSuccessGetNotes();
     }
