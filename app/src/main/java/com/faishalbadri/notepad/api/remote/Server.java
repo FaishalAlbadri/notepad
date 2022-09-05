5a3c56fbb4d9c9aa4cdfaa429fa6af3e6e994619
Faishalbadri <faishalalbadri1@gmail.com>
2022-09-05 18:46:33 +0700

    change api to new api

--- app/src/main/java/com/faishalbadri/notepad/api/remote/Server.java
+++ app/src/main/java/com/faishalbadri/notepad/api/remote/Server.java
@@ -2,7 +2,7 @@ package com.faishalbadri.notepad.api.remote;
 
 public class Server {
 
-    public final static String BASE_URL = "https://quran-apii.herokuapp.com/";
+    public final static String BASE_URL = "http://27.112.78.93:5000/";
     public final static String CHECK_INTERNET_CONNECTION = "Check your internet connection";
 
 }
